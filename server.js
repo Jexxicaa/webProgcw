c9bfec43be8e285b3301cfeb37deb01481aa480e
Jessica Busby <jessicabusby211@gmail.com>
2023-05-09 10:10:22 +0100

    serverclient

--- server.js
+++ server.js
@@ -1,7 +1,32 @@
+import express from 'express';
+import sqlite3 from 'sqlite3';
+import * as db from './entries.js';
 
- import express from 'express';
+db.initialise();
 
 const app = express();
 app.use(express.static('client'));
 app.listen(8080);
-console.log('Running on http://localhost:8080 to stop do Ctrl+C'); 
+console.log('Running on http://localhost:8080 to stop do Ctrl+C');
+
+// open and close db
+const mydb = new sqlite3.Database('./database.sql', sqlite3.OPEN_READWRITE, (err) => {
+  if (err) { 
+    return console.error(err.message); }
+  console.log('Connected');
+});
+
+// close database
+mydb.close((err) => { 
+    if (err) { 
+        return console.error(err.message); }
+  console.log('Disconnected');
+});
+
+//root to get entries + add new one
+
+app.get('/entry', gettheentries);
+
+function gettheentries (req,res){
+  res.json(db.getAllEntries());
+}
