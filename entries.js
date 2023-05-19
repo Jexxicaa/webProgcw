96608991f6e446da62a71511fb749ee9f7a3445c
Jessica <jessicabusby211@gmail.com>
2023-05-19 14:45:33 +0100

    Made read only + added documentation

--- entries.js
+++ entries.js
@@ -1,26 +1,27 @@
 import fs from 'fs/promises';
 
+//create entries array
 let entries = [];
 
 export async function initialise (){
-    const data = await fs.readFile('entries.json');
-    entries = JSON.parse(data);
+    const data = await fs.readFile('entries.json'); // Read entries from json file
+    entries = JSON.parse(data); // Parse JSON data and assign to the entries array
 }
 
 export function getAllEntries (){
-    return entries;
+    return entries; // Return all entries in entries array
 }
 
-//function to add to the array
 export function addToArray (date, work, knowledge, competencies){
+    // Add a entry to array
     entries.push({date,work,knowledge,competencies});
     
 }
 
-//write to disk so its stored
 async function writeToDisk() {
+    // Append updated array to JSON file
       await fs.appendFile('entries.json', entries);
 }
   
 
-writeToDisk();
+writeToDisk(); // Write the entries to disk
